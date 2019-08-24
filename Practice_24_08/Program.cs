24d5a2fa536542d1b9994977e2d7b224251147a6
dimitar.nedilkovski@gmail.com <dimitar.nedilkovski@gmail.com>
2019-08-24 09:51:35 +0300

    Добавьте файлы проекта.

---
+++ Practice_24_08/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practice_24_08
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
